1a1ac00587ed6200687424c66b131eb962272315
MaksTomsic <maks.tomsic@gmail.com>
2025-08-13 14:03:26 +0200

    prvi commit za frontend(input osnovno)

---
+++ resources/js/input.component.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+// Sprejme besedilo za label kot prop
+const props = defineProps<{
+  namen: string
+}>()
+
+// Shranjuje vpisano besedilo
+const text = ref('')
+</script>
+
+<template>
+  <label for="prostorček">{{ props.namen }}</label>
+  <input v-model="text" placeholder="Tukaj začni pisati: " id="prostorček">
+</template>
+
+<style scoped>
+input {
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  padding: 6px;
+}
+</style>
