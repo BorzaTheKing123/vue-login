72f85b26896db72b09650ed5f0189eea02d0bcc2
Samo Borzatta <samo.borzatta@gmail.com>
2025-08-18 14:02:12 +0200

    Hitr backend za AddClients

--- app/Http/Controllers/StrankeController.php
+++ app/Http/Controllers/StrankeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class StrankeController extends Controller
 {
     public function index()
     {
         return "hello";
+    }
+
+    public function create()
+    {
+        return Inertia::render('AddClients', ['id' => Auth::id()]);
     }
 }
